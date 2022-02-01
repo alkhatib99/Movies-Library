fb3b5fb74c1ae37055f571da33aa20016c1e1125
Alkhatib99 <aboodjob@gmail.com>
2022-02-01 06:44:05 +0200

    Task12

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Movies-Library - version 1
 
 **Author Name**: Abedalqader Alkhatib.
-**Date**: 1/31/2022.
+**Date**: 1/31/2022. - 2/1/2022
 
 ## WRRC
 
@@ -9,6 +9,9 @@ Web Request Response Cycle
 
 This is an simple drawing of WRRC.
 !image['The Wrrc Drawing'](./images/wrrc.jpg)
+
+Another simple drawing of WRRC.
+!image['Drawing WRRC'](./images/wrrc2.jpg)
 
 ## Overview
 
@@ -25,5 +28,12 @@ This app provides a two error handler, one for not found page (404), other for s
 This app provides handler forread data.json and define constructor of Movie include title, poster path and overview, then handle for home pagee '/'.
 
 Also handler for favorite page'/favorite'.
+handler for not found pages, using a promise function to fill data from api
+new features:
+
+- add handler for trending page.
+- add handler for search page.
+- add handler for latest page.
+- add handler for person page.
 
 ....Will continue.
--- server.js
+++ server.js
@@ -1,51 +1,120 @@
+require('dotenv').config();
 const express= require('express');
 const cors = require('cors');
-
+const axois = require('axios');
 const app=express();
 app.use(cors());
+const port=process.env.PORT;
+const apikey=process.env.APIKEY;
 
-
-
+let urlPerson=`https://api.themoviedb.org/3/person/popular?api_key=${process.env.APIKEY}`;
 let movieData = require('./MovieData/data.json');
+// const json = require('express/lib/response');
+// const url=require(`https://api.themoviedb.org/3/movie/76341?api_key=${apikey}`);
+///// endpoints 
+//axois.get(url);
 
-///// endpoints ///server for food recipes 
-
+app.listen(port,()=>
+{
+    console.log('listening to port 3000');
+});
+ 
 app.get('/',movieHandler);
-app.get('/favorite',favoritePage);
-app.get('*',notFoundHndler);
+app.get('/person',peoplePage);
+app.get('/trending',trendingPage);
+app.get('/search', searchPage)
+app.get('/search', letestPage)
+app.use('*',notFoundHndler);
 //constructor
+Person.allpersons=[];
 
-
-function Movie(title,posterPath,overView){
-    
-    this.recipetitle = title;
-    this.posterPath=posterPath;
-    this.overView=overView;
+function Movie(title, poster_path, overview)
+{
+this.title=title;
+this.poster_path=poster_path;
+this.overview=overview;
+}
+function Movie(id,title,release_date,poster_path,overview){
+    this.id=id;
+    this.title = title;
+    this.release_date=release_date;
+    this.poster_path=poster_path;
+    this.overview=overview;
     
 }
-
-
-function favoritePage(req,res)
+function Person(id,name,profile_path)
 {
-return res.status(200).send(" Welcome to favorite Page");
+    this.id=id;
+    this.name=name;
+    this.profile_path=profile_path;
+}
+function movieHandler(req,res){
+
+    let movie = { title:movieData.title, poster_path:movieData.poster_path,overview:movieData.overview};
+console.log("movie");
+return res.status(200).json(movie);
+}
+function searchPage(req,res)
+{
+    axois.get(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=Hulk`).then(data=>{
+    
+        let movies =data.data.results.map(elem=>{return  { id:elem.id,title:elem.title,release_date:elem.release_date,poster_path:elem.poster_path,overview:elem.overview};});
+    return res.status(200).json(movies);
+}).catch(err=>{
+    return res.status(401).send(err);
+    })
+}
+function trendingPage(req, res)
+{
+    axois.get(urlPerson).then(data=>{
+        let movies =data.data.results.map(elem=>{return  {id:elem.id,title:elem.title,release_date:elem.release_date,poster_path:elem.poster_path,overview:elem.overview};});
+        return res.status(200).json(movies);
+}).catch(err=>{
+console.log(err);
+return res.status(500).send(err);
+    });
+}
+
+function peoplePage(req,res)
+{
+
+    axois.get(urlPerson)
+    .then((data)=>{
+        // console.log(data.data);
+        let res=data.data.results;
+// console.log(res);
+      let persons=  res.map((elm)=>{
+        return {id:elm.id,name:elm.name,profile_path:elm.profile_path}; 
+        
+        //  console.log(elm.id);
+      });
+
+   
+console.log(persons);
+      return  res.status(200).json(persons);
+
+    }).catch(err=>{
+
+
+    });
 };
 
 
 
-function movieHandler(req,res){
+function letestPage(req,res){
 
-        let movie = new Movie(    movieData.title, movieData.poster_path, movieData.overview);
-console.log("movie");
-    return res.status(200).json(movie);
+axois.get(`https://api.themoviedb.org/3/movie/latest?api_key=${apikey}`).then(data=>{
+
+  //  console.log(data);
+  let movies =data.data.results.map(elem=>{return new Movie(elem.id,elem.title,elem.release_date,elem.poster_path,elem.overview);});
+    return res.status(200).json(movies);}).catch(err=>{
+return res.status(401).send(err);
+});
+
 }
 
 function notFoundHndler(req,res){
     
-    return res.status(404).responseText("huuuh?");
+    return res.status(404).send("huuuh?");
 
 };
-
-app.listen(8000,()=>
-{
-    console.log('listening to port 8000');
-});
